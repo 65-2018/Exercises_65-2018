8314264b9488552af6d5b264c1e198c5d1f2a23c
unknown <nidzaasa@gmail.com>
2020-11-09 18:01:02 +0100

    Implementirano sve

---
+++ Exercise_65-2018/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Exercise_65_2018
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxExerciseResults = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBoxExerciseResults
+            // 
+            this.listBoxExerciseResults.FormattingEnabled = true;
+            this.listBoxExerciseResults.ItemHeight = 15;
+            this.listBoxExerciseResults.Location = new System.Drawing.Point(12, 27);
+            this.listBoxExerciseResults.Name = "listBoxExerciseResults";
+            this.listBoxExerciseResults.Size = new System.Drawing.Size(207, 214);
+            this.listBoxExerciseResults.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Results";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(231, 254);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBoxExerciseResults);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Exercise results";
+            this.Load += new System.EventHandler(this.Ucitaj);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxExerciseResults;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Exercise_65-2018/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise_65_2018
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection connection { get; set; }
+        private List<ExerciseResult> results { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+            results = new List<ExerciseResult>();
+        }
+
+        private void Ucitaj(object sender, EventArgs e)
+        {
+            connection = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=FacultyDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            connection.Open();
+
+            var query = @"SELECT Id, StudentName, StudentIndex, Points FROM ExerciseResults";
+            var cmd = new SqlCommand(query, connection);
+
+            var reader = cmd.ExecuteReader();
+
+            while(reader.Read())
+            {
+                var id = reader.GetInt32(reader.GetOrdinal("Id"));
+                var name = reader.GetString(reader.GetOrdinal("StudentName"));
+                var index = reader.GetString(reader.GetOrdinal("StudentIndex"));
+                var points = reader.GetInt32(reader.GetOrdinal("Points"));
+
+                results.Add(new ExerciseResult(id, name, index, points));
+            }
+
+            listBoxExerciseResults.Items.Clear();
+
+            foreach (var item in results)
+                listBoxExerciseResults.Items.Add(item);
+
+            connection.Close();
+        }
+    }
+}
